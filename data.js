0b10d10e6ac069644c1cb767a35f27712c261bd7
Riyanim14 <riyanimtiyaz7@gmail.com>
2025-06-18 09:46:53 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,152 @@
+const audioSentenceData = [
+    {
+        "id": 1,
+        "english": "Now listen to the net",
+        "urdu": "اب نیٹ پہ سن دیتے ہیں",
+        "audio": "1.mp3"
+    },
+    {
+        "id": 2,
+        "english": "In addition to the labor, government and business people start work at 9-10",
+        "urdu": "مزدور تپے کے علاوہ سرکاری اور کاروباری لوگ 9-10 پہ جے کام شروع کرتے ہیں",
+        "audio": "2.mp3"
+    },
+    {
+        "id": 3,
+        "english": "General Mushtab goes",
+        "urdu": "جنرل مشرب جاتے ہیں",
+        "audio": "3.mp3"
+    },
+    {
+        "id": 4,
+        "english": "Is the interest of the United States",
+        "urdu": "دلچسٹ امریا ہے",
+        "audio": "4.mp3"
+    },
+    {
+        "id": 5,
+        "english": "It will not be said that three divisions of a book given by Zaid have occurred",
+        "urdu": "یہ نہیں کہا جائے گا کہ زید کی طرف سے دی گئی ایک بک کی تین طلاقے واقعہ ہو گئی ہیں",
+        "audio": "5.mp3"
+    },
+    {
+        "id": 6,
+        "english": "Rickom on time never returned",
+        "urdu": "وقت پر رکوم کبھی بھی واپس نہ کی",
+        "audio": "6.mp3"
+    },
+    {
+        "id": 7,
+        "english": "Drawli Rankings Mohammad Abbas's best",
+        "urdu": "ڈرہیلی رینکنگ محمد عباس کی کریر کی بہترین پوڑیشن",
+        "audio": "7.mp3"
+    },
+    {
+        "id": 8,
+        "english": "West Africa",
+        "urdu": "مغربی افریقہ",
+        "audio": "8.mp3"
+    },
+    {
+        "id": 9,
+        "english": "Where the world's most high",
+        "urdu": "جہاں دنیا کا سب سے اونچا",
+        "audio": "9.mp3"
+    },
+    {
+        "id": 10,
+        "english": "It will be",
+        "urdu": "ایسا ہوگا",
+        "audio": "10.mp3"
+    },
+    {
+        "id": 11,
+        "english": "What will happen then?",
+        "urdu": "پھر کیا ہوگا؟",
+        "audio": "11.mp3"
+    },
+    {
+        "id": 12,
+        "english": "The most important aspect of the governors is the present and this",
+        "urdu": "گورنرس کا سب سے اہم پہلو موجود اور عزازان کی اسلاح ہے",
+        "audio": "12.mp3"
+    },
+    {
+        "id": 13,
+        "english": "Were not very happy with the government",
+        "urdu": "حکومت سے بہت خوش نہیں تھے",
+        "audio": "13.mp3"
+    },
+    {
+        "id": 14,
+        "english": "If they meet them, I will be able to do so",
+        "urdu": "اگر ان سے ملاقات ہو تو میری طرف تصلیم عرض کیجئے گا",
+        "audio": "14.mp3"
+    },
+    {
+        "id": 15,
+        "english": "These are the months of God and the winter",
+        "urdu": "یہی خدا اور سردیوں کے مہینے ہیں",
+        "audio": "15.mp3"
+    },
+    {
+        "id": 16,
+        "english": "That the light of Surah continued to reach them",
+        "urdu": "کہ ان کے اندر سورت کی روشنی پہنچتی رہے",
+        "audio": "16.mp3"
+    },
+    {
+        "id": 17,
+        "english": "Of the rich and the growing rank of the whole world",
+        "urdu": "محولیاتی علود کی اور پوری دنیا کے بڑھتے ہوئے درجہ ہر آرت",
+        "audio": "17.mp3"
+    },
+    {
+        "id": 18,
+        "english": "The only thing to indicate to the man",
+        "urdu": "اکل من کو اشارہ ہی کافی",
+        "audio": "18.mp3"
+    },
+    {
+        "id": 19,
+        "english": "What are the francs doing in Berlin?",
+        "urdu": "اُروا و فران برلین میں کیا کرہے ہیں؟",
+        "audio": "19.mp3"
+    },
+    {
+        "id": 20,
+        "english": "Two teams were created",
+        "urdu": "دو ٹیمیں بنائی گئیں",
+        "audio": "20.mp3"
+    },
+    {
+        "id": 21,
+        "english": "I thought I should meet him on a public max",
+        "urdu": "میں نے سوچا مجھے اسے کسی پبلک مکام پر ملنا چاہیئے",
+        "audio": "21.mp3"
+    },
+    {
+        "id": 22,
+        "english": "How did Nawaz Sarif succeed in politics?",
+        "urdu": "نواز سریف سیاست میں کیسے کامیاب ہوئے؟",
+        "audio": "22.mp3"
+    },
+    {
+        "id": 23,
+        "english": "The environment of the residential retailers should be abandoned to make the mosquito rush.",
+        "urdu": "کہ مچھ روبار تھنڈا کرنے کے لیے رفریجی ریٹرز کا ماحول دشمن طریقہ ترک کیا جائے",
+        "audio": "23.mp3"
+    },
+    {
+        "id": 24,
+        "english": "How in the coming decades too",
+        "urdu": "کیسے آنے والے دہائوں میں اس لئے بھی",
+        "audio": "24.mp3"
+    },
+    {
+        "id": 25,
+        "english": "I'm there",
+        "urdu": "وہاں جھڑپیں چاری ہوں",
+        "audio": "25.mp3"
+    }
+];
